5a1ab1be1fadab44444bf44a231de1b5c2cd0ddf
supercarsaresick <122555323+supercarsaresick@users.noreply.github.com>
2023-01-20 12:08:05 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -105,6 +105,9 @@ input.onButtonPressed(Button.A, function () {
         . # # # .
         `)
 })
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showString("Whoa!")
+})
 input.onButtonPressed(Button.AB, function () {
     basic.showLeds(`
         # # . # #
@@ -194,7 +197,7 @@ input.onButtonPressed(Button.B, function () {
         `)
 })
 input.onGesture(Gesture.Shake, function () {
-    music.playMelody("C D E D F F E D ", 120)
+    music.playMelody("C D E D F F G D ", 120)
     basic.showString("Video games are interesting!")
     basic.showLeds(`
         # # # # #
@@ -204,6 +207,9 @@ input.onGesture(Gesture.Shake, function () {
         . # . # .
         `)
 })
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showString("Now that is a better view!")
+})
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showLeds(`
         . # . # .
@@ -212,7 +218,51 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
         # . . . #
         # # # # #
         `)
+    basic.showString("What sport do you play?")
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        # # . # #
+        # . # . #
+        . # # # .
+        `)
+    basic.showString("Soccer")
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # . . . #
+        # . # . #
+        . # # # .
+        `)
+    basic.showString("Basketball")
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showString("Baseball")
+    basic.showLeds(`
+        . # # # .
+        . # # # .
+        . # # # .
+        . . # . .
+        . . # . .
+        `)
+    basic.showString("Tennis")
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        `)
+    basic.showString("Swimming")
+    basic.showString("Or etcetera...")
 })
+basic.showString("Welcome to the fidget cube pseudocode!")
+basic.showString("Press the various buttons to begin.")
 basic.forever(function () {
 	
 })
